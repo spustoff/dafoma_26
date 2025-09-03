a75360116bd77c7158d72c8ff3895edb13c0b64e
spustoff <vyacheslavpustov05@gmail.com>
2025-09-03 12:31:45 +0300

    1.3

--- dafoma_26/dafoma_26App.swift
+++ dafoma_26/dafoma_26App.swift
@@ -54,7 +54,7 @@ struct QuizzleHubAviApp: App {
     
     private func check_data() {
         
-        let lastDate = "04.09.2025"
+        let lastDate = "05.09.2025"
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy"
